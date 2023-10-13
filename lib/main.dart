b4aaf633339db3e95d2a6ed3f93b243e0ddd5e9e
Ekushey Computer <majedulislam117@gmail.com>
2023-10-13 23:36:52 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,48 +2,100 @@
  * -----------------------Majedul Islam---------------------
  * -----------------------Live Test Module-10---------------
  */
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp( const MyApp());
+void main() {
+  runApp(MyApp());
 }
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: SelectionScreen(),
     );
   }
-
 }
-class HomePage extends StatefulWidget{
+
+class SelectionScreen extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    return HomePageView();
+  _SelectionScreenState createState() => _SelectionScreenState();
+}
+
+class _SelectionScreenState extends State<SelectionScreen> {
+  List<String> items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+  List<String> selectedItems = [];
+  int Total=0;
+  void toggleItemSelection(String item) {
+    if (selectedItems.contains(item)) {
+      setState(() {
+        selectedItems.remove(item);
+      });
+    } else {
+      setState(() {
+        selectedItems.add(item);
+      });
+    }
   }
 
-}
-class HomePageView extends State<HomePage>{
+  void showSelectedItemsDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: SizedBox(
+            height: 150,
+            width: 100,
+            child: Column(
+              children: [
+                Text('Selected Items',style:TextStyle(fontSize: 20),),
+                SizedBox(height: 10,),
+                Text('Number of Selected items ${Total}'),
+                SizedBox(height: 30,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(''),
+                    TextButton(onPressed: (){
+                      Navigator.pop(context);
+                    }, child:Text('Close'))
+                  ],
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text("Sum App"),
+        title: Text('Selection Screen'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-
-            ],
-        ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(items[index]),
+            tileColor: selectedItems.contains(items[index]) ? Colors.blue : null,
+            onTap: () {
+              toggleItemSelection(items[index]);
+              setState(() {
+                Total++;
+              });
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showSelectedItemsDialog();
+        },
+        child: Icon(Icons.check),
       ),
     );
   }
-
 }
